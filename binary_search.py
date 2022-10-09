685b7393ab56108c4bbd2986562631282f4fb2c4
Isaack Mungui <munguiisaack@gmail.com>
2022-10-09 10:39:19 +0300

    Added binary search functionality

--- binary_search.py
+++ binary_search.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-
-from bisect import bisect_left
 import file_handle
 
-def binary_search(words_list, word):
+def binary_search(words_list, low, high, word):
 
     # TODO: Analyze every word in the file using binary search
     # TODO: If word not found in dictionary, print it on the screen as potentially incorrect
 
-    i = bisect_left(words_list, word)
-    if i != len(words_list) and words_list[i] == word:
-        return i
+    if high >= low:
+        mid = (high + low) // 2
+
+        if words_list[mid] == word:
+            return mid
+        elif words_list[mid] > word:
+            return binary_search(words_list, low, mid - 1, word)
+        else:
+            return binary_search(words_list, mid + 1, high, word)
     else:
         return -1
 
 # call the open_file method from file_handle module
-words = file_handle.open_file('dictionary.txt', 'r')
-
-# Prompt for user input
-word = str(input("Enter word:"))
-res = binary_search(words, word)
+words_list = file_handle.open_file('dictionary.txt', 'r')
+word = str(input("Enter word: "))
+res = binary_search(words_list, 0, len(words_list) - 1, word)
 if res == -1:
     print(word, "is absent")
 else:
-    print("First occurrence of", word, "is present at", res)
+    print("First occurrence of", word, "is present at index", res)
